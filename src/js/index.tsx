2750f43313013f3b8c938aebcc45cd382688f73e
古宮信二 <>
2026-02-10 16:02:04 +0900

    jsxの基本

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -3,5 +3,26 @@ import { createRoot } from 'react-dom/client'
 
 document.body.innerHTML = '<div id="app"></div>'
 
+const todoItem = "掃除と洗濯"
+// const element = <h1 className='hello'>Hello, World</h1>
+
+const handleClick = () => {
+    alert("ボタンがクリックされました")
+}
+
+const element = (
+  <div>
+    <h1>Hello, World</h1>
+    <p>Reactの学習をしていきましょう&#xff01;</p>
+    <p>{todoItem}</p>
+    <button onClick={handleClick}>ボタン</button>
+  </div>
+)
+
+// const element = <h1>Task: {todoItem}</h1>
+
+
+
+
 const root = createRoot(document.getElementById('app')!)
-root.render(<h1>Hello, World</h1>)
+root.render(element)
