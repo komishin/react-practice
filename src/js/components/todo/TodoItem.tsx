9d696eb2226b2f5df7eb5321b8840370de90131d
古宮信二 <>
2026-02-13 17:00:26 +0900

    データを削除できるように実装

--- src/js/components/todo/TodoItem.tsx
+++ src/js/components/todo/TodoItem.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import { Todo } from './type'
 
 type TodoItemProps = {
+  id: number
   task: string
   person: string
   deadline: string
+  setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
 // const TodoItem: React.FC<TodoItemProps> = ({task, deadline}) => {
-export const TodoItem = ({ task, person, deadline }: TodoItemProps) => {
+export const TodoItem = ({
+  id,
+  task,
+  person,
+  deadline,
+  setTodoList,
+}: TodoItemProps) => {
   return (
-    <li className="grid grid-cols-3">
+    <li className="grid grid-cols-4 pb-1">
       <div>{task}</div>
       <div>{person}</div>
       <div>締め切り：{deadline}</div>
+      <div>
+        <button
+          className="border bg-red-400 w-16 rounded"
+          //filter=今の条件に合うものだけを残す（クリックして一致したidのもの以外残す）
+          onClick={() => setTodoList((prev) => prev.filter((todo) => todo.id !== id))}
+        >
+          削除
+        </button>
+      </div>
     </li>
   )
 }
