bbc911c5ffb304380b2b7d00e7362a168b32d3ef
古宮信二 <>
2026-02-15 08:59:40 +0900

    ログイン名と担当者名が一致している場合は、色とフォントを変える

--- src/js/components/todo/TodoItem.tsx
+++ src/js/components/todo/TodoItem.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Button } from '../parts/Button'
 import { AuthContext } from '../../contexts/AuthContext'
+import { useAuth } from '../../contexts/use-auth'
 
 type TodoItemProps = {
   id: number
@@ -18,21 +19,19 @@ export const TodoItem = ({
   deadline,
   deleteTodo,
 }: TodoItemProps) => {
-    const { isLoggedIn } = useContext(AuthContext)
-
+  const { userName } = useAuth()
+  const style = userName === person ? 'text-red-600 font-bold' : ''
 
   return (
     <li className="grid grid-cols-4 pb-1">
       <div>{task}</div>
-      <div>{person}</div>
+      <div className={style}>{person}</div>
       <div>締め切り：{deadline}</div>
       <div>
         {/* filter=今の条件に合うものだけを残す（クリックして一致したidのもの以外残す） */}
         <Button onClick={() => deleteTodo(id)} color="red">
           削除
         </Button>
-              <div>{isLoggedIn ? 'ログイン中' : 'ログアウト中'}</div>
-
       </div>
     </li>
   )
--- src/js/components/todo/TodoList.tsx
+++ src/js/components/todo/TodoList.tsx
@@ -12,26 +12,23 @@ export const TodoList = ({ todoList, deleteTodo }: Props) => {
   const { isLoggedIn } = useContext(AuthContext)
 
   return (
-    <>
-      <ul className="bg-emerald-100 p-4 rounded">
-        <li className="grid grid-cols-4 font-bold">
-          <div>タスク名</div>
-          <div>担当者名</div>
-          <div>締め切り</div>
-          <div>削除</div>
-        </li>
-        {todoList.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            id={todo.id}
-            task={todo.task}
-            person={todo.person}
-            deadline={todo.deadline}
-            deleteTodo={deleteTodo}
-          />
-        ))}
-      </ul>
-      <div>{isLoggedIn ? 'ログイン中' : 'ログアウト中'}</div>
-    </>
+    <ul className="bg-emerald-100 p-4 rounded">
+      <li className="grid grid-cols-4 font-bold">
+        <div>タスク名</div>
+        <div>担当者名</div>
+        <div>締め切り</div>
+        <div>削除</div>
+      </li>
+      {todoList.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          task={todo.task}
+          person={todo.person}
+          deadline={todo.deadline}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
   )
 }
