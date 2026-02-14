49f584504af079634948ae8154e26135a2a8993b
古宮信二 <>
2026-02-15 08:19:54 +0900

    ログイン、ログアウト状態を作成

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -5,22 +5,42 @@ import { NewTodoForm } from './components/todo/NewTodoForm'
 import { Todo } from './components/todo/type'
 import { useTodoList } from './components/todo/use-todoList'
 import { AuthContext } from './contexts/AuthContext'
+import { TextField } from './components/parts/TextField'
+import { Button } from './components/parts/Button'
 
 export const App = () => {
   const { todoList, addTodo, deleteTodo } = useTodoList()
 
-  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+  const { isLoggedIn, setIsLoggedIn, userName, setUserName } =
+    useContext(AuthContext)
+
+  if (!isLoggedIn)
+    return (
+      <main className="my-0 mx-auto w-4/5 text-center">
+        <Heading level="h1">ログイン</Heading>
+        <div className='flex gap-1'>
+          <TextField
+            id="user-name"
+            label="ユーザー名"
+            type="text"
+            value={userName}
+            onChange={setUserName}
+          ></TextField>
+          <Button onClick={() => setIsLoggedIn(true)} color="blue">
+            ログイン
+          </Button>
+        </div>
+      </main>
+    )
 
   return (
     <main className="my-0 mx-auto w-4/5 text-center">
       <Heading level="h1">TODO</Heading>
-      <div>{isLoggedIn ? 'ログイン中' : 'ログアウト中'}</div>
+      <div>{userName}</div>
       <div>
-        <input
-          type="checkbox"
-          checked={isLoggedIn}
-          onClick={() => setIsLoggedIn(!isLoggedIn)}
-        />
+          <Button onClick={() => setIsLoggedIn(false)} color="red">
+            ログアウト
+          </Button>
       </div>
       <div className="mt-8">
         <Heading level="h2">新規TODO作成</Heading>
