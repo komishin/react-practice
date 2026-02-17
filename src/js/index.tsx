0e1c020f2a5d169cb7b222d0fd42972dd74fdc5b
古宮信二 <>
2026-02-17 11:38:54 +0900

    ChakraProviderの導入

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -3,32 +3,13 @@ import { createRoot } from 'react-dom/client'
 import '../css/main.css'
 import { App } from './App'
 import { AuthProvider } from './contexts/AuthContext'
-
-// document.body.innerHTML = '<div id="app"></div>'
-
-// const todoItem = "掃除と洗濯"
-// // const element = <h1 className='hello'>Hello, World</h1>
-
-// const handleClick = () => {
-//     alert("ボタンがクリックされました")
-// }
-
-// const element = (
-//   <div>
-//     <h1>Hello, World</h1>
-//     <p>Reactの学習をしていきましょう&#xff01;</p>
-//     <p>{todoItem}</p>
-//     <button onClick={handleClick}>ボタン</button>
-//   </div>
-// )
-
-// const element = <h1>Task: {todoItem}</h1>
-
-// ReactDOM.render(<TodoItem task="掃除" deadline="明日まで"/>, document.getElementById('root'))
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = createRoot(document.getElementById('app')!)
 root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <ChakraProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ChakraProvider>
 )
