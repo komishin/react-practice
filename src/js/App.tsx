6bf8f54c7e0f8758692d39a5eaca5e9e3dc51c86
古宮信二 <>
2026-02-18 16:30:38 +0900

    詳細ページを表示できるように変更

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -7,12 +7,14 @@ import {
   Route,
 } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
+import { TodoDetail } from './pages/TodoDetail'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/login" element={<Login />} />
       <Route path="/todo" element={<Todo />} />
+      <Route path="/todo/:id" element={<TodoDetail />} />
     </>
   )
 )
---
+++ src/js/pages/TodoDetail.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react"
+import React from "react"
+
+
+export const TodoDetail = () =>{
+    return <Box>Todoの詳細ページ</Box>
+}
